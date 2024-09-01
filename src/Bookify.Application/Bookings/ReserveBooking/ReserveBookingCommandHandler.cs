9dab8212c2887dd634550afb54af21359ee1c5a5
Lucas Alixandre Lima <lucas.alixandre@cacaushow.com.br>
2024-09-01 16:59:43 -0300

    implemented first handle

---
+++ src/Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Users;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBookingCommand, Guid>
+{
+    private readonly IUserRepository _userRepository;
+    public readonly  IApartmentRepository _apartmentRepository;
+    public readonly  IBookingRepository _bookingRepository;
+    public readonly  IUnitOfWork _unitOfWork;
+    public readonly  PricingService _pricingService;
+
+    public ReserveBookingCommandHandler(IUserRepository userRepository,
+                                       IApartmentRepository apartmentRepository,
+                                       IBookingRepository bookingRepository, 
+                                       IUnitOfWork unitOfWork, 
+                                       PricingService pricingService)
+    {
+        _userRepository = userRepository;
+        _apartmentRepository = apartmentRepository;
+        _bookingRepository = bookingRepository;
+        _unitOfWork = unitOfWork;
+        _pricingService = pricingService;
+    }
+
+    public async Task<Result<Guid>> Handle(ReserveBookingCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
