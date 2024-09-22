01ea947caccb2e0b0bf1270b1c10b8a50d938daf
Lucas Alixandre Lima <lucas.alixandre@cacaushow.com.br>
2024-09-22 18:12:56 -0300

    implemented configuration entity

---
+++ src/Bookify.Infrastructure/Clock/DateTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Bookify.Application.Abstractions.Clock;
+
+namespace Bookify.Infrastructure.Clock
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
---
+++ src/Bookify.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure.Database;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options) 
+    {
+        
+    }
+}
